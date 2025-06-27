6028d22ebfb16af14b806aece0d3eb266552ff45
theaihopgg <theaihopgg@gmail.com>
2025-06-27 22:05:36 +0700

    fix: execute

--- core/database.py
+++ core/database.py
@@ -6,6 +6,6 @@ from core.config import DATABASE_FILENAME
 async def create_tables() -> None:
     async with db_connect(DATABASE_FILENAME) as conn:
         async with conn.cursor() as cur:
-            await cur.execute(open("./sql/create_tables.sql").read())
+            await cur.executescript(open("./sql/create_tables.sql").read())
 
         await conn.commit()
