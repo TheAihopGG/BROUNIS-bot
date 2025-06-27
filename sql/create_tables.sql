4884063a3bc1da4837d800075c045cd5b5e6c015
theaihopgg <theaihopgg@gmail.com>
2025-06-27 22:18:13 +0700

    fix: create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS report_tickets (
+    id INTEGER PRIMARY KEY,
+    channel_id INTEGER,
+    user_discord_id INTEGER,
+    target_discord_id INTEGER,
+    report_message VARCHAR(200)
+);
+CREATE TABLE IF NOT EXISTS report_ticket_members (
+    id INTEGER PRIMARY KEY,
+    user_discord_id INTEGER,
+    report_ticket_id INTEGER,
+    FOREIGN KEY (report_ticket_id) REFERENCES report_tickets(id)
+);
+CREATE INDEX IF NOT EXISTS report_ticket_id_index ON report_ticket_members(id);
+CREATE INDEX IF NOT EXISTS report_ticket_member_id_index ON report_ticket_members(id);
