8eafa0226cfdd258d59f05a05f00a6472870c4ab
theaihopgg <theaihopgg@gmail.com>
2025-06-29 00:20:18 +0700

    feat: added guild_settings

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -14,6 +14,12 @@ CREATE TABLE IF NOT EXISTS report_ticket_moderator_roles (
     id INTEGER PRIMARY KEY,
     role_id INTEGER
 );
+CREATE TABLE IF NOT EXISTS guild_settings (
+    id INTEGER PRIMARY KEY,
+    guild_id INTEGER UNIQUE,
+    report_tickets_category_id INTEGER DEFAULT NULL
+);
 CREATE INDEX IF NOT EXISTS report_ticket_id_index ON report_ticket_members(id);
 CREATE INDEX IF NOT EXISTS report_ticket_member_id_index ON report_ticket_members(id);
 CREATE INDEX IF NOT EXISTS report_ticket_moderator_role_id_index ON report_ticket_moderator_roles(id);
+CREATE INDEX IF NOT EXISTS guild_settings_id_index ON guild_settings(id);
